b3c56aace37c0df528ddd6d428c5b6d6357fd0ca
woffordlm <lukewofford@gmail.com>
2021-12-10 12:01:52 -0500

    comments added

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -1,6 +1,8 @@
+// these global variables pull data from local storage and grab the clear button element
 var savedHighScores = JSON.parse(localStorage.getItem("highscores")) || [];
 var clearButton = document.getElementById("clear-scores")
 
+// as soon as the page loads this function cycles through the array from local storage and displays them on the page
 window.onload = function(){
     for (let i = 0; i < savedHighScores.length; i++) {
         var listedScore = savedHighScores[i].score
@@ -14,12 +16,13 @@ window.onload = function(){
         table.appendChild(listedItem);
     }
 }
+// this funciton allows the user to clear all data in the table by deleting it from local storage
 function clearHighScores(event){
     console.log("heyyyy")
     window.localStorage.removeItem("highscores");
     location.reload();
 }
-// button not working
 
+// this event listener triggers the clearHighScores function
 clearButton.addEventListener("click",clearHighScores);
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+
+// Initial setup of global variables
 var startButtonEl = document.getElementById("start-button");
 var mainEl = document.getElementById("page-content");
 var choiceList = document.getElementById("choice-list");
@@ -5,9 +7,11 @@ var questionindex = 0
 var wrappperEl = document.getElementById("prompt-wrapper-id");
 var validationEl = document.createElement("div");
 var questionEl = document.getElementById("question-place-id");
+var scoreLinkEl = document.getElementById("score-link");
+// these variables set the timer and player score
 var timeLeft = 60;
 var score = 0
-var scoreLinkEl = document.getElementById("score-link");
+// thi variable acts the storage for all of the question, choices, and answers
 var questions = [
     {
         title: "Which answer displays the proper way to add a class name to an element? ",
@@ -35,29 +39,27 @@ var questions = [
         answer: "Javascript"
     }
   ]  ;
-var savedScores= []
-
+// this array is eventually filled out and then pushed to local storage
+  var savedScores= []
+// this function acts as the countdown timer that is triggered by a click event
 function countdown(event) {
     var timerEl = document.getElementById('countdown');
-  
-    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    // this variable will decrease the timer every second until it reaches 0
     var timeInterval = setInterval(function () {
-      // As long as the `timeLeft` is greater than 1
       if (timeLeft >= 1) {
-        // Set the `textContent` of `timerEl` to show the remaining seconds
         timerEl.textContent = 'time: '+ timeLeft;
-        // Decrement `timeLeft` by 1
         timeLeft--;
       } else {
-        // Once `timeLeft` gets to 0, set `timerEl` to an empty string
         timerEl.textContent = '';
-        // Use `clearInterval()` to stop the timer
+        // this function stops the timer
         clearInterval(timeInterval);
       }
     }, 1000);
 }
+
+// this function is called by the clearScreen function
+// thhis funtion cycles through the questions array to display all the questions and answers
 function callQuestions(){
-       // initially populate question field
         questionEl.textContent = "";
         var quesEl = questions[questionindex].title;
         questionEl.textContent = quesEl;
@@ -73,60 +75,69 @@ function callQuestions(){
                 choiceList.appendChild(listed); 
             }
 }
+// this function triggers the end of the game
+// when the parameters are meet the screen will display a new stucture and will 
+// ask the user to input their initials, this data is then saved in local storage along with the score
+// the user has the option to start the quiz again by using the restart button
 function endGame(){
     if(timeLeft===0 || questionindex===5){
-            questionEl.innerHTML = "";
-            choiceList.innerHTML= "";
-            var doneEl = document.createElement("div");
-            doneEl.className = "end-game-title"
-            doneEl.textContent= "Finished"
-            var doneParagraphEl = document.createElement("p");
-            doneParagraphEl.className = "end-game-paragraph";
-            doneParagraphEl.textContent = "You finished with a score of "+score+" points";
-            var inputEl = document.createElement("input");
-            inputEl.className = "end-game-input";
-            var endGame = document.getElementById("endgame");
-            var buttonEl = document.createElement("button");
-            buttonEl.className = "end-button";
-            var restartEl = document.createElement("button");
-            restartEl.textContent = "Restart";
-            restartEl.className = "end-button";
-            buttonEl.textContent= "Submit"
+        // this section clears content form the page and feel it back in with elements
+        // instructing the user to fill out the input
+        questionEl.innerHTML = "";
+        choiceList.innerHTML= "";
+        var doneEl = document.createElement("div");
+        doneEl.className = "end-game-title"
+        doneEl.textContent= "Finished"
+        var doneParagraphEl = document.createElement("p");
+        doneParagraphEl.className = "end-game-paragraph";
+        doneParagraphEl.textContent = "You finished with a score of "+score+" points";
+        var inputEl = document.createElement("input");
+        inputEl.className = "end-game-input";
+        var endGame = document.getElementById("endgame");
+        var buttonEl = document.createElement("button");
+        buttonEl.className = "end-button";
+        var restartEl = document.createElement("button");
+        restartEl.textContent = "Restart";
+        restartEl.className = "end-button";
+        buttonEl.textContent= "Submit"
             
-            endGame.appendChild(doneEl);
-            endGame.appendChild(doneParagraphEl);
-            endGame.appendChild(inputEl);
-            endGame.appendChild(buttonEl);
-            endGame.appendChild(restartEl);
-            timeLeft= 0
-
-            restartEl.addEventListener("click", function(){
-                questionindex=0
-                timeLeft= 60
-                endGame.innerHTML= ""
-                callQuestions();
+        endGame.appendChild(doneEl);
+        endGame.appendChild(doneParagraphEl);
+        endGame.appendChild(inputEl);
+        endGame.appendChild(buttonEl);
+        endGame.appendChild(restartEl);
+        timeLeft= 0
+        // this event listener is linked to a button that triggers a function
+        // to reset the game
+        restartEl.addEventListener("click", function(){
+            questionindex=0
+            timeLeft= 60
+            endGame.innerHTML= ""
+            callQuestions();
             })
-
-
-            buttonEl.addEventListener("click", function(){
-                // console.log(score);
-                var initials = document.querySelector("input");
-                // console.log(initials.value);
-                var scoreObject = {
-                    score: score,
-                    initials: initials.value
+        // This button triggers a function that saves the score and inital to local storage
+        buttonEl.addEventListener("click", function(){
+            // console.log(score);
+            var initials = document.querySelector("input");
+            // console.log(initials.value);
+            var scoreObject = {
+                score: score,
+                initials: initials.value
                 }
                 
-                savedScores.push(scoreObject);
-                localStorage.setItem("highscores",JSON.stringify(savedScores));
-               
-            })
-          
+            savedScores.push(scoreObject);
+            localStorage.setItem("highscores",JSON.stringify(savedScores));       
+            })     
     }
     else{
         callQuestions();
     }      
 }
+// this funtion checks for the correct answers by matching the target button value with the value of 
+// the correct answer in the questions array
+// a set timer is enabled so that user has time to view if the answer was correct before moving on
+// points are added or deducted depending on the answer
+// timeLeft is deducted by 10 seconds if the answer is wrong
 function validation(event){
             if(event.target.matches("button")){
                     var answerEl= questions[questionindex].answer;
@@ -143,7 +154,7 @@ function validation(event){
                         questionEl.textContent=""
                         questionindex++
                         endGame();
-                        },2000)
+                        },1000)
                     }
 
                     else if(answerEl !== confirmAnswer)  {
@@ -157,16 +168,11 @@ function validation(event){
                         questionEl.textContent=""
                         questionindex++
                         endGame();
-                        },2000)
-    
-                
-            }
-          
+                        },1000)      
+            } 
         } 
-     
-    
-    
 }
+// this function clears the inital screen making way for the question and answer elements
 function clearScreen(){
      // remove paragraph
      var paragraph = document.getElementById("initial-p");
